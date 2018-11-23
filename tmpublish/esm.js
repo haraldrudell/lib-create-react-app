f8b95cd9167731f3e472a3d118e57f06d421ff53
Harald Rudell <harald.rudell@gmail.com>
2018-11-23 02:43:29 -0800

    before implementing ./publish

---
+++ tmpublish/esm.js
@@ -0,0 +1,8 @@
+var App$1 = App = () => 'text';
+
+/*
+© 2018-present Harald Rudell <harald.rudell@gmail.com> (http://www.haraldrudell.com)
+This source code is licensed under the ISC-style license found in the LICENSE file in the root directory of this source tree.
+*/
+
+export { App$1 as App };
